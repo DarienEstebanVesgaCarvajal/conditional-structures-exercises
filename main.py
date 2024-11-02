19b05b0b4e66831132bbe5d3cbb3f329f223b971
DarienEstebanVesgaCarvajal <182381184+DarienEstebanVesgaCarvajal@users.noreply.github.com>
2024-11-02 15:27:23 -0500

    leap-year

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-try:
-    number = int(input("what's the number?: "))
-    if number % 2 == 0:
-        print("The number is even.")
+year = int(input("What's yhe year?: "))
+
+if year >= 1582: # G
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+            print(f"The {year} year is a leap.")
     else:
-        print("The number is odd.")
-except ValueError:
-    print("The number is invalid.")
+          print(f"The {year} year isn't a leap.")
+else:# J
+    if year %4 == 0:
+            print(f"The {year} year is a leap.")
+    else:
+        print(f"The {year} year isn't a leap.")
